5b9ba0285bf0702f2797d148c886604a206009ad
zhangjunming123 <1187742595@qq.com>
2021-11-20 17:06:53 +0800

    create project

---
+++ src/main/java/com/nowcoder/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String hello(){
+        return "Hello world!";
+    }
+}
